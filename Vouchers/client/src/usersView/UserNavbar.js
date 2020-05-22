421f1feca57907e2e6939e081c32968dff0ec34a
Tomás Accini <taccini@fb.com>
2020-05-22 16:00:23 -0300

    Added Navbar

---
+++ Vouchers/client/src/usersView/UserNavbar.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button from "components/CustomButtons/Button.js";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "left"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  signOutButton: {
+    color: 'white',
+  }
+}));
+
+
+export default function DenseAppBar(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <div className="root">
+      <AppBar position="fixed">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu" onClick={handleClick}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            {props.title}
+          </Typography>
+          <Button className="signOutButton" simple>
+            Cerrar sesión
+          </Button>
+        </Toolbar>
+        <Drawer
+          className={classes.drawer}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          open={open}
+          keepMounted
+          onClose={handleClose}
+        >
+          <div className={classes.toolbar}>
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={handleClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <List>
+              <ListItem button key={"Comprar Vouchers"}>
+                <ListItemIcon><ShoppingCartIcon/></ListItemIcon>
+                <ListItemText primary={"Comprar Vouchers"} />
+              </ListItem>
+              <ListItem button key={"Canjear mis Vouchers"}>
+                <ListItemIcon><StorefrontIcon/></ListItemIcon>
+                <ListItemText primary={"Canjear mis Vouchers"} />
+              </ListItem>
+            </List>
+          </div>
+        </Drawer>
+      </AppBar>
+    </div>
+  );
+}
--- Vouchers/client/src/usersView/UsersView.js
+++ Vouchers/client/src/usersView/UsersView.js
@@ -1,14 +1,76 @@
 import React, {Component} from 'react';
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import VouchersList from "./VouchersList.js"
-
-
+import VouchersList from "./VouchersList.js";
+import UserNavbar from "./UserNavbar.js";
+import "./styles.css";
 
 class UsersView extends Component {
     getListOfVouchers() {
         const l = [
             {
+                "Title": "2 Hamburguesas",
+                "Description": "Vale por 2 hamburguesas con queso",
+                "Price": 600,
+                "CreationDate": "2020-04-01",
+                "EndDate": "2020-06-01",
+                "Stock": 4
+            },
+            {
+                "Title": "2 Cervezas",
+                "Description": "Vale por 2 cervezas",
+                "Price": 150,
+                "CreationDate": "2020-04-10",
+                "EndDate": "2020-12-31",
+                "Stock": 100
+            },
+            {
+                "Title": "Sala de escape",
+                "Description": "Vale por una visita a nuestras salas de escape",
+                "Price": 300,
+                "CreationDate": "2020-05-01",
+                "EndDate": "2020-08-10",
+                "Stock": 1
+            },
+            {
+                "Title": "10 paquetes de yerba",
+                "Description": "Vale por 10 paquetes de yerba",
+                "Price": 1000,
+                "CreationDate": "2020-05-10",
+                "EndDate": "2020-10-01",
+                "Stock": 30
+            },{
+                "Title": "2 Hamburguesas",
+                "Description": "Vale por 2 hamburguesas con queso",
+                "Price": 600,
+                "CreationDate": "2020-04-01",
+                "EndDate": "2020-06-01",
+                "Stock": 4
+            },
+            {
+                "Title": "2 Cervezas",
+                "Description": "Vale por 2 cervezas",
+                "Price": 150,
+                "CreationDate": "2020-04-10",
+                "EndDate": "2020-12-31",
+                "Stock": 100
+            },
+            {
+                "Title": "Sala de escape",
+                "Description": "Vale por una visita a nuestras salas de escape",
+                "Price": 300,
+                "CreationDate": "2020-05-01",
+                "EndDate": "2020-08-10",
+                "Stock": 1
+            },
+            {
+                "Title": "10 paquetes de yerba",
+                "Description": "Vale por 10 paquetes de yerba",
+                "Price": 1000,
+                "CreationDate": "2020-05-10",
+                "EndDate": "2020-10-01",
+                "Stock": 30
+            },{
                 "Title": "2 Hamburguesas",
                 "Description": "Vale por 2 hamburguesas con queso",
                 "Price": 600,
@@ -47,11 +109,14 @@ class UsersView extends Component {
 
     render() {
         return (
-            <GridContainer>
-                <GridItem>
-                    <VouchersList vouchers={this.getListOfVouchers()}/>
-                </GridItem>
-            </GridContainer>
+            <div>
+                <UserNavbar />
+                <GridContainer className="vouchersGrid" >
+                    <GridItem>
+                        <VouchersList vouchers={this.getListOfVouchers()}/>
+                    </GridItem>
+                </GridContainer>
+            </div>
         );
     }
 }
