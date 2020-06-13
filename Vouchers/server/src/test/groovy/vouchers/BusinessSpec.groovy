00d0710e311b66abf7ade21f45033dd84ed61bd1
Tomás Accini <taccini@fb.com>
2020-06-13 18:29:50 -0300

    add buisenss unit tests

--- Vouchers/server/src/test/groovy/vouchers/BusinessSpec.groovy
+++ Vouchers/server/src/test/groovy/vouchers/BusinessSpec.groovy
@@ -11,8 +11,16 @@ class BusinessSpec extends Specification implements DomainUnitTest<Business> {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "constructor"() {
+        Business b = new Business(name: "Burger", phone_number: "123412341234", address: new Address(street: "Libertador", number: "1234", country: new Country(name: "Argentina")), category: "Restaurant", email: "burger@gmail.com", password: "burger1234")
+        expect:"business constructed correctly"
+        b != null && b.name == "Burger" && b.email == "burger@gmail.com" && b.password == "burger1234" && b.phone_number == "123412341234" && b.category == "Restaurant" && !b.verified_account
+    }
+
+    void "verify account"() {
+        Business b = new Business(name: "Burger", phone_number: "123412341234", address: new Address(street: "Libertador", number: "1234", country: new Country(name: "Argentina")), category: "Restaurant", email: "burger@gmail.com", password: "burger1234")
+        b.verify_account()
+        expect:"account is verified"
+        b.verified_account
     }
 }
