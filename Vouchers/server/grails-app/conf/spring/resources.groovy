db2dc54ce222650e616d0a20da5dc3e66b9754ab
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2020-06-15 14:23:32 -0300

    Updates resources.groovy with new beans

--- Vouchers/server/grails-app/conf/spring/resources.groovy
+++ Vouchers/server/grails-app/conf/spring/resources.groovy
@@ -1,6 +1,19 @@
+import assemblers.CounterfoilAssembler
+import assemblers.ItemAssembler
 import assemblers.ProductAssembler
+import assemblers.VoucherInformationAssembler
 
 // Place your Spring DSL code here
 beans = {
     productAssembler(ProductAssembler){}
+
+    itemAssembler(ItemAssembler) {
+        productAssembler = ref("productAssembler")
+    }
+
+    voucherInformationAssembler(VoucherInformationAssembler){}
+
+    counterfoilAssembler(CounterfoilAssembler) {
+        voucherInformationAssembler = ref("voucherInformationAssembler")
+    }
 }
