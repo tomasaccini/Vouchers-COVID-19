88982fed9e47a6dd8593ee57cb405ec217f6d63a
Florencia-97 <florrr1997@gmail.com>
2020-06-17 01:02:03 -0300

    Refactor over Counterfoil

--- Vouchers/server/grails-app/domain/vouchers/Counterfoil.groovy
+++ Vouchers/server/grails-app/domain/vouchers/Counterfoil.groovy
@@ -15,18 +15,8 @@ class Counterfoil {
         vouchers cascade: 'save-update'
     }
     static constraints = {
-        voucherInformation blank: false, nullable: false
-        stock blank: false, nullable: false, default: 0
+        voucherInformation      blank: false, nullable: false
+        stock                   blank: false, nullable: false, default: 0
     }
 
-    Voucher createVoucher() {
-        if (stock <= 0) {
-            throw new RuntimeException("Voucher does not have stock")
-        }
-        Voucher v = new Voucher(voucherInformation: voucherInformation.duplicate(), dateCreated: new Date())
-        v.setCounterfoil(this)
-        addToVouchers(v)
-        stock -= 1
-        v
-    }
 }
--- Vouchers/server/grails-app/services/vouchers/CounterfoilService.groovy
+++ Vouchers/server/grails-app/services/vouchers/CounterfoilService.groovy
@@ -7,9 +7,15 @@ class CounterfoilService {
 
     VoucherService voucherService
 
-   Voucher createVoucher(Long id){
-       Counterfoil counterfoil = Counterfoil.get(id)
-       Voucher voucher = voucherService.createVoucher(counterfoil.voucherInformation)
-       return voucher
-   }
+    Voucher createVoucher(Long id) {
+        Counterfoil counterfoil = Counterfoil.get(id)
+        if (counterfoil.stock <= 0) {
+            throw new RuntimeException("Voucher does not have stock")
+        }
+        Voucher v = voucherService.createVoucher(counterfoil.voucherInformation)
+        counterfoil.addToVouchers(v)
+        //TODO: This must be propably an atomic attribute
+        counterfoil.stock -= 1
+        v
+    }
 }
