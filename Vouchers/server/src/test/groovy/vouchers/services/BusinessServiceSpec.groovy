602a9c481ba41734614a579f4fbcbbfd87787d75
Tomás Accini <taccini@fb.com>
2020-06-18 01:28:34 -0300

    fix BusinessServiceSpec

--- Vouchers/server/src/test/groovy/vouchers/services/BusinessServiceSpec.groovy
+++ Vouchers/server/src/test/groovy/vouchers/services/BusinessServiceSpec.groovy
@@ -1,12 +1,16 @@
 package vouchers.services
 
+import enums.ProductType
 import enums.states.VoucherState
 import grails.gorm.transactions.Rollback
 import grails.testing.mixin.integration.Integration
+import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
 import vouchers.Address
 import vouchers.Business
+import vouchers.BusinessService
 import vouchers.Client
+import vouchers.ClientService
 import vouchers.Counterfoil
 import vouchers.Country
 import vouchers.Item
@@ -18,60 +22,98 @@ import vouchers.VoucherInformation
 @Rollback
 class BusinessServiceSpec extends Specification{
 
+    @Autowired
+    BusinessService businessService
+    ClientService clientService
+
+    private static Boolean setupIsDone = false
+    static Long businessId
+    static Long clientId
+
     def setup() {
+        if (setupIsDone) return
+
+        Business business = new Business()
+        business.name = "Blue Dog"
+        business.email = "sales@bluedog.com"
+        business.password = "1234"
+        business.verifiedAccount = true
+        business.phone_number = "1234"
+        business.category = "Cervezería"
+        Address newAddress = new Address()
+        newAddress.street = "calle falsa"
+        newAddress.number = "123"
+        newAddress.apartment = "11D"
+        newAddress.province = "Buenos Aires"
+        Country country = new Country()
+        country.name = "Argentina"
+        newAddress.country = country
+
+        business.address = newAddress
+        business.website = "bluedog.com"
+
+        Product product = new Product()
+        product.description = "Hamburguesa con cebolla, cheddar, huevo, jamón, todo."
+        product.name = "Hamburguesa Blue Dog"
+        product.type = ProductType.FAST_FOOD
+        business.addToProducts(product)
+        business.save(flush: true, failOnError: true)
+
+        Item item = new Item(product: product, quantity: 1)
+        VoucherInformation vi = new VoucherInformation(price: 400, description: "Promo verano", validFrom: new Date('2020/08/01'), validUntil:  new Date('2020/08/15'))
+        vi.addToItems(item)
+        Counterfoil counterfoil = new Counterfoil(voucherInformation: vi, stock: 3, isActive: true)
+        business.addToCounterfoils(counterfoil)
+        business.save(flush: true, failOnError: true)
+
+        Client client = new Client(fullName: "Ricardo Fort", email: "ricki@gmail.com", password: "ricki1234")
+        client.verifiedAccount = true
+        client.save(flush:true, failOnError:true)
+
+        setupIsDone = true
+        clientId = client.id
+        businessId = business.id
     }
 
     def cleanup() {
     }
 
-    def createVoucherInformation() {
-        Product p1 = new Product(name:"Hamburguesa", description: "Doble cheddar")
-        Product p2 = new Product(name:"Pinta cerveza", description: "Cerveza artesanal de la casa")
-        Item i1 = new Item(product: p1, quantity: 1)
-        Item i2 = new Item(product: p2, quantity: 2)
-
-        VoucherInformation vi = new VoucherInformation(price: 400, description: "Promo verano", validFrom: new Date('2020/08/01'), validUntil: new Date('2020/12/31'), items: [i1, i2])
-        vi
+    void "setup test"() {
+        expect:
+        Business.count() == 1
+        Product.count() == 1
+        Counterfoil.count() == 1
     }
 
     void "confirm voucher retirement"() {
-        Client client = new Client(full_name: "Ricardo Fort", email: "ricki@gmail.com", password: "ricki1234")
-        Business b = new Business(name: "Burger", phone_number: "123412341234", address: new Address(street: "Libertador", number: "1234", country: new Country(name: "Argentina")), category: "Restaurant", email: "burger@gmail.com", password: "burger1234")
-        VoucherInformation vi = createVoucherInformation()
-        Counterfoil counterfoil = new Counterfoil(voucherInformation: vi, stock: 3)
-        b.addCounterfoil(counterfoil)
-        Voucher v = client.buyVoucher(counterfoil)
-        client.retireVoucher(v)
-        b.confirmRetireVoucher(v)
+        Counterfoil counterfoil = Counterfoil.findById(1)
+        Voucher v = clientService.buyVoucher(clientId, counterfoil)
+        Client client = Client.get(clientId)
+        clientService.retireVoucher(clientId, v)
+        businessService.confirmRetireVoucher(businessId, v)
         expect:"Vouchers status in retired"
         v != null && client.getVouchers().size() == 1 && client.getVouchers()[0] == v && v.getState() == VoucherState.RETIRED
     }
 
     void "confirm voucher retirement before client retires it"() {
-        Client client = new Client(full_name: "Ricardo Fort", email: "ricki@gmail.com", password: "ricki1234")
-        Business b = new Business(name: "Burger", phone_number: "123412341234", address: new Address(street: "Libertador", number: "1234", country: new Country(name: "Argentina")), category: "Restaurant", email: "burger@gmail.com", password: "burger1234")
-        VoucherInformation vi = createVoucherInformation()
-        Counterfoil counterfoil = new Counterfoil(voucherInformation: vi, stock: 3)
-        b.addCounterfoil(counterfoil)
-        Voucher v = client.buyVoucher(counterfoil)
+        Counterfoil counterfoil = Counterfoil.findById(1)
+        Voucher v = clientService.buyVoucher(clientId, counterfoil)
         when:
-        b.confirmRetireVoucher(v)
+        businessService.confirmRetireVoucher(businessId, v)
         then: "Throw error"
         thrown RuntimeException
         v.state == VoucherState.BOUGHT
     }
 
     void "confirm voucher from other business"() {
-        Client client = new Client(full_name: "Ricardo Fort", email: "ricki@gmail.com", password: "ricki1234")
-        Business b1 = new Business(name: "Burger", phone_number: "123412341234", address: new Address(street: "Libertador", number: "1234", country: new Country(name: "Argentina")), category: "Restaurant", email: "burger@gmail.com", password: "burger1234")
         Business b2 = new Business(name: "Mc", phone_number: "123412341234", address: new Address(street: "Corrientes", number: "1234", country: new Country(name: "Argentina")), category: "Restaurant", email: "mc@gmail.com", password: "mc1234")
-        VoucherInformation vi = createVoucherInformation()
-        Counterfoil counterfoil = new Counterfoil(voucherInformation: vi, stock: 3)
-        b1.addCounterfoil(counterfoil)
-        Voucher v = client.buyVoucher(counterfoil)
-        client.retireVoucher(v)
+        b2.save()
+        Counterfoil counterfoil = Counterfoil.findById(1)
+        Voucher v = clientService.buyVoucher(clientId, counterfoil)
+        Client client = Client.get(clientId)
+        clientService.retireVoucher(clientId, v)
         when:
-        b2.confirmRetireVoucher(v)
+        businessService.confirmRetireVoucher(b2.id, v)
         then: "Throw error"
         thrown RuntimeException
         v.state == VoucherState.PENDING_CONFIRMATION
