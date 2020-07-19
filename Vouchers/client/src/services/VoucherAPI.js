a97f82e6ff8115a75d0d1ae0d30b79ec1f08517d
Rodrigo Souto <rnsoutob@gmail.com>
2020-07-19 20:24:20 -0300

    Creating VoucherAPI

---
+++ Vouchers/client/src/services/VoucherAPI.js
@@ -0,0 +1,46 @@
+import {SERVER_URL} from '../config';
+
+
+class VoucherAPI {
+  async getCounterfoils(userId) {
+    const url = `${SERVER_URL}recommendations`;
+    console.log(`debug | getCounterfoils URL is: ${url}`);
+    const response = await fetch(url);
+    const counterfoils = await response.json();
+    return counterfoils.map((c) => this._transformCounterfoil(c));
+  }
+
+  _transformCounterfoil(counterfoil) {
+    console.log(`debug | _transformCounterfoil ${JSON.stringify(counterfoil)}`);
+    const vi = counterfoil.voucherInformation;
+    return {
+      // TODO change title, maybe something with items !!!!
+      'Title': '2 Hamburguesas',
+      'Description': vi.description,
+      'Price': vi.price,
+      // TODO format dates from 2020-08-01T03:00:00Z to 2020-08-01
+      'CreationDate': vi.validFrom,
+      'EndDate': vi.validUntil,
+      'Stock': counterfoil.stock,
+      // TODO no mor owner !!!!
+      'isOwned': false,
+      // !!!!
+      'shopName': 'Tio Felipe'
+    }
+
+    return {
+      'Title': '2 Hamburguesas',
+      'Description': 'Vale por 2 hamburguesas con queso',
+      'Price': 600,
+      'CreationDate': '2020-04-01',
+      'EndDate': '2020-06-01',
+      'Stock': 4,
+      'isOwned': false,
+      'shopName': 'Tio Felipe'
+    }
+  }
+}
+
+const voucherAPI = new VoucherAPI();
+
+export default voucherAPI;
