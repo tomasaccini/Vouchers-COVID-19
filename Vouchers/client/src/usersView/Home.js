60ef0b9d0a3848d238dc772dd701c2d76257aacd
Florencia-97 <florrr1997@gmail.com>
2021-01-10 15:18:11 -0300

    Adds icon to home page

--- Vouchers/client/src/usersView/Home.js
+++ Vouchers/client/src/usersView/Home.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import covidIcon from '../images/covid.svg';
 import '../css/home.css';
 
 class Home extends Component {
     render(){
         return(
             <section className="home-page">
+                <img className="rotate covid" src={covidIcon} alt="covid"/>
                 <h1 className="home-title">Covid - Vouchers</h1>
                 <p className="home-stitle">
                     Ayuda a tu local Amigo comprando vouchers de tus productos preferidos
