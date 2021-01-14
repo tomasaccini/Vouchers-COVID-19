781b8e162d532782b4f83f6a66b0a1f3e98b0a86
tomasaccini <tomasaccini@gmail.com>
2021-01-14 14:53:22 -0300

    test para buscar talonarios con descripcion en negocio con talonarios sin descripcion

--- Vouchers/server/src/test/groovy/vouchers/domain/NegocioSpec.groovy
+++ Vouchers/server/src/test/groovy/vouchers/domain/NegocioSpec.groovy
@@ -3,11 +3,22 @@ package vouchers.domain
 import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 import vouchers.Direccion
+import vouchers.InformacionVoucher
+import vouchers.Item
 import vouchers.Negocio
+import vouchers.Producto
+import vouchers.Talonario
 
 class NegocioSpec extends Specification implements DomainUnitTest<Negocio> {
 
-    def cleanup() {
+    def crearInformacionVoucher(descripcion = null) {
+        Producto p1 = new Producto(nombre: "Hamburguesa", descripcion: "Doble cheddar")
+        Producto p2 = new Producto(nombre: "Pinta cerveza", descripcion: "Cerveza artesanal de la casa")
+        Item i1 = new Item(producto: p1, cantidad: 1)
+        Item i2 = new Item(producto: p2, cantidad: 2)
+
+        InformacionVoucher iv = new InformacionVoucher(precio: 400, descripcion: descripcion, validoDesde: new Date('2020/08/01'), validoHasta: new Date('2030/12/31'), items: [i1, i2])
+        iv
     }
 
     void "constructor"() {
@@ -23,10 +34,19 @@ class NegocioSpec extends Specification implements DomainUnitTest<Negocio> {
         n.cuentaVerificada
     }
 
-    void "buscar talonarios sin descripcion en negocio sin talonarios"() {
+    void "buscar talonarios con descripcion en negocio sin talonarios"() {
         Negocio n = new Negocio(nombre: "Burger", numeroTelefonico: "123412341234", direccion: new Direccion(calle: "Libertador", numero: "1234", pais: "Argentina"), categoria: "Restaurant", email: "burger@gmail.com", contrasenia: "burger1234")
-        expect: "la cuenta esta verificada"
+        expect: "el negocio no tiene talonarios con descripcion"
         !n.tieneTalonarioConDescripcion()
     }
 
+    void "buscar talonarios con descripcion en negocio con talonarios sin descripcion"() {
+        InformacionVoucher iv = crearInformacionVoucher()
+        Negocio n = new Negocio(nombre: "Burger", numeroTelefonico: "123412341234", direccion: new Direccion(calle: "Libertador", numero: "1234", pais: "Argentina"), categoria: "Restaurant", email: "burger@gmail.com", contrasenia: "burger1234")
+        n.save()
+        Talonario talonario = new Talonario(stock: 6, informacionVoucher: iv, negocio: n)
+        talonario.save()
+        expect: "el negocio no tiene talonarios con descripcion"
+        !n.tieneTalonarioConDescripcion()
+    }
 }
