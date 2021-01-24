9ee2e95aa20e5178920f7be80ee6cf981daf6e0b
Rodrigo Souto <rnsoutob@gmail.com>
2021-01-24 03:29:35 -0300

    Corrigiendo RecomendadorTalonarios para que use el rating posta

--- Vouchers/server/grails-app/domain/vouchers/InformacionVoucher.groovy
+++ Vouchers/server/grails-app/domain/vouchers/InformacionVoucher.groovy
@@ -2,11 +2,8 @@ package vouchers
 
 class InformacionVoucher {
 
-    private final RATING_DEFAULT = 3
-
     double precio
     String descripcion // descripcion general del cupón
-    Long rating
     Date validoDesde
     Date validoHasta
     Set<Item> items = []
@@ -23,7 +20,6 @@ class InformacionVoucher {
         descripcion blank: false, nullable: false
         validoDesde blank: false, nullable: false
         validoHasta blank: false, nullable: false
-        rating nullable: true
     }
 
     // TODO Isn't there something like deep clone not to do this manually?
@@ -36,13 +32,5 @@ class InformacionVoucher {
         }
 
         new InformacionVoucher(precio: precio, descripcion: descripcion, validoDesde: validoDesde, validoHasta: validoHasta, items: nuevosItems)
-    }
-
-    Long obtenerRating() {
-        if (rating == null) {
-            return RATING_DEFAULT
-        }
-
-        return rating
     }
 }
--- Vouchers/server/src/main/groovy/services/RecomendadorTalonarios.groovy
+++ Vouchers/server/src/main/groovy/services/RecomendadorTalonarios.groovy
@@ -6,6 +6,7 @@ class RecomendadorTalonarios {
 
     private final Double FACTOR_CANTIDAD_VENDIDA = 0.6
     private final Double FACTOR_RATING = 0.4
+    private final Long RATING_DEFAULT_COLD_START = 3
     private final Double MAX_RATING = 5
 
     TalonarioService talonarioService
@@ -26,12 +27,14 @@ class RecomendadorTalonarios {
 
     private Tuple2<Talonario, Double> generarTalonarioConPuntajeEnBaseAlasDimensionesPonderadas(Talonario talonario, Long maxVendidos) {
         println("!!!! " + talonario.negocio.nombre  + ": " + calcularPuntajeEnBaseAlasDimensionesPonderadas(talonario, maxVendidos))
+        println("!!!! " + talonario.negocio.nombre  + ": " + calcularPuntajeDimensionCantidadVendida(talonario.cantidadVendida(), maxVendidos))
+        println("!!!! " + talonario.negocio.nombre  + ": " + calcularPuntajeDimensionRating(talonario.obtenerRating()))
         return new Tuple2(talonario, calcularPuntajeEnBaseAlasDimensionesPonderadas(talonario, maxVendidos))
     }
 
     private Double calcularPuntajeEnBaseAlasDimensionesPonderadas(Talonario talonario, Long maxVendidos) {
         return calcularPuntajeDimensionCantidadVendida(talonario.cantidadVendida(), maxVendidos) +
-                calcularPuntajeDimensionRating(talonario.informacionVoucher.obtenerRating())
+                calcularPuntajeDimensionRating(talonario.obtenerRating())
     }
 
     private Double calcularPuntajeDimensionCantidadVendida(Long cantidadVendida, Long maxVendidos) {
@@ -39,6 +42,7 @@ class RecomendadorTalonarios {
     }
 
     private Double calcularPuntajeDimensionRating(Long rating) {
-        return FACTOR_RATING * rating / MAX_RATING
+        Long ratingCorregido = rating != 0 ? rating : RATING_DEFAULT_COLD_START
+        return FACTOR_RATING * ratingCorregido / MAX_RATING
     }
 }
