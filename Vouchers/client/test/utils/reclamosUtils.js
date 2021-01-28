5f797fda79260e7eacbbbe29994ad9a7ac6b5110
tomasaccini <tomasaccini@gmail.com>
2021-01-28 01:53:10 -0300

    agrego funcion para iniciar reclamo

--- Vouchers/client/test/utils/reclamosUtils.js
+++ Vouchers/client/test/utils/reclamosUtils.js
@@ -3,6 +3,24 @@ async function voucherPuedeIniciarReclamo(page, descripcion) {
   return elemento.length > 0;
 }
 
+async function iniciarReclamo(page, descripcion, primerMensaje) {
+  const iniciarReclamoSelector = `//h2[text()="${descripcion}"]/../..//span[@id="iniciarReclamo"]`;
+  const descripcionIniciarReclamoSelector = `//h5[contains(text(), "${descripcion}")]/../..//input[@id="descripcionReclamo"]`;
+  const confirmarIniciarReclamoSelector = `//h5[contains(text(), "${descripcion}")]/../../..//button[@id="confirmarInicioReclamo"]`;
+  await page.waitForXPath(iniciarReclamoSelector);
+  const iniciarReclamoBoton = await page.$x(iniciarReclamoSelector);
+  await iniciarReclamoBoton[0].click();
+  await page.waitForTimeout(2000);
+  await page.waitForXPath(descripcionIniciarReclamoSelector);
+  await page.waitForXPath(confirmarIniciarReclamoSelector);
+  const descripcionIniciarReclamo = await page.$x(descripcionIniciarReclamoSelector);
+  await descripcionIniciarReclamo[0].click();
+  await page.keyboard.type(primerMensaje);
+  const confirmarIniciarReclamo = await page.$x(confirmarIniciarReclamoSelector);
+  await confirmarIniciarReclamo[0].click();
+}
+
 module.exports = {
-  voucherPuedeIniciarReclamo
+  voucherPuedeIniciarReclamo,
+  iniciarReclamo
 };
